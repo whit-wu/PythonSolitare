48010caec50246800cf4ec27ebef7c4936cdec36
whit-wu <whit.wu@gmail.com>
2017-02-06 22:43:58 -0500

    Update Deck.py

--- Deck.py
+++ Deck.py
@@ -12,8 +12,11 @@ class Deck(object):
     def createDeck(self):
         SUITS = u'\u2660\u2663\u2665\u2666'
         RANKS = '23456789TJQKA'
+        # declaration of deck tuple.  uses for loop to populate a tuple
         DECK = tuple(''.join(card) for card in itertools.product(RANKS, SUITS))
+        # converts the tuple to a list.
         randDeck = list(DECK)
+        # for loop to shuffle the deck.
         for x in range(0, 1000):
             random.shuffle(randDeck)
         return randDeck
